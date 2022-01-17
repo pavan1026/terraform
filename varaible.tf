e0d7cb3d5a3d54ae8cdddfcc89c513f7068ab1a4
pavan1026 <52605330+pavan1026@users.noreply.github.com>
2022-01-17 01:58:55 +0000

    112

--- varaible.tf
+++ varaible.tf
@@ -2,3 +2,13 @@ variable "ec2_instance"{
 type = string
 default = "us-east-1"
 }
+
+variable "provider_token" {
+  type = string
+  sensitive = true
+  default = "3cGcNrlJqlSErg.atlasv1.zvwrYlUfF5wpLftPv9fXNFiYiBrDezBI3QmBm8ErYiJD88936QEBZWqy1SdOKSxCqiQ"
+}
+
+provider "fakewebservices" {
+  token = var.provider_token
+}
